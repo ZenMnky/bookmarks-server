d6dcd4c178b474da33157e49366aeb9121b7e2e8
Justin Hager <coachjustinhager@gmail.com>
2020-12-07 12:34:16 -0800

    adds seed file

---
+++ seeds/seed.bookmark_list.sql
@@ -0,0 +1,8 @@
+INSERT INTO bookmark_list
+(id, title, url, description, rating)
+VALUES
+ (1, 'fake news', 'fakeurl', 'this is fake stuff', 3),
+ (2, 'fake news', 'fakeurl', 'this is fake stuff', 3),
+ (3, 'fake news', 'fakeurl', 'this is fake stuff', 3),
+ (4, 'fake news', 'fakeurl', 'this is fake stuff', 3),
+ (5, 'fake news', 'fakeurl', 'this is fake stuff', 3);
